815d6a5fd2410bfa58379a90f2b31ccc875ea6dd
dpepper <pepper.daniel@gmail.com>
2019-06-06 17:28:35 -0700

    better path support and validation.  wildcard support

--- pluckit/pluckit.py
+++ pluckit/pluckit.py
@@ -5,15 +5,13 @@ from types import *
 __all__ = [ 'pluckit' ]
 
 
-split_path = re.compile(r'\.|\[|\]').split
-
 
 def pluckit(obj, handle):
     if obj is None or handle is None:
         # nothing to pluck
         return None
 
-    # function pointer
+    # function pointer, behave like map()
     if callable(handle):
         return handle(obj)
 
@@ -21,55 +19,117 @@ def pluckit(obj, handle):
     if isinstance(handle, int):
         return obj[handle]
 
-    # strange...just, pass it through
+    # unsupported, just pass it through
     if not isinstance(handle, str):
         return obj[handle]
 
-    res = obj
-    for next in split_path(handle):
-        if not next:
-            continue
+    return pluckPath(obj, handle)
 
-        if str.isdigit(next):
-            # cast to int
-            res = pluckPathItem(res, int(next))
-        elif next[0] == next[-1] and (next[0] in ['"', "'"]):
-            # strip quotes
-            res = pluckPathItem(res, next[1:-1])
+
+path_regex = re.compile(
+    r'^((?P<dot>\.)?(?P<handle>\*|-?\d+|[a-z_][a-z0-9_]*)|\[(?P<subscript>.+?)\])(?=[\*\.\[]|\Z)',
+    flags=re.IGNORECASE
+)
+is_numeric = re.compile(r'^-?\d+$').match
+
+def pluckPath(obj, path):
+    res = obj
+
+    while path:
+        match = path_regex.match(path)
+
+        if not match:
+            raise ValueError('invalid path syntax: ' + path)
+
+        # trim element from remaining path
+        path = path[len(match.group()):]
+
+        # an explicit attribute or method
+        attr = bool(match.group('dot'))
+
+        # an explicit subscript
+        subscript = bool(match.group('subscript'))
+
+        handle = match.group('handle') or match.group('subscript')
+
+
+        if '*' == handle:
+            # match all values
+
+            if hasattr(res, 'values'):
+                values = res.values()
+            else:
+                values = iter(res)
+
+            if path:
+                # pluck remaining path from all values
+                res = [ pluckit(x, path) for x in values ]
+
+                if len(res) == 0 and not path_regex.match(path):
+                    # corner case were path needs validation,
+                    # eg pluckit([], '*.')
+                    raise ValueError('invalid path syntax: ' + path)
+            else:
+                # match all remaining values
+                res = list(values)
+
+            # break out of loop since pluck completed via recursion
+            return res
+
+        elif attr:
+            # explicit object attribute or method
+            res = pluckAttr(res, handle)
+
+        elif subscript:
+            # explicit subscript index, eg. for dict or list
+
+            if is_numeric(handle):
+                # list-like
+                handle = int(handle)
+            elif handle[0] == handle[-1] and (handle[0] in ['"', "'"]):
+                # strip quotes
+                handle = handle[1:-1]
+
+            res = res[handle]
+
         else:
-            # use as is
-            res = pluckPathItem(res, next)
+            # unspecified type, use heuristics.
+            # only possible on first path element
+            assert obj == res
+
+            if hasattr(res, 'keys'):
+                res = res[handle]
+
+            elif hasattr(res, handle):
+                # seems legit since attr exists, so go with it
+                res = pluckAttr(res, handle)
+
+            elif hasattr(res, '__getitem__'):
+                # assume handle is subscript
+
+                if is_numeric(handle):
+                    # list-like index
+                    res = res[int(handle)]
+                else:
+                    # dict-like index
+                    res = res[handle]
+
+            else:
+                raise ValueError('invalid handle: ' + handle)
 
     return res
 
 
+def pluckAttr(obj, handle):
+    attr = getattr(obj, handle)
 
-def pluckPathItem(obj, handle):
-    # index for list-like object
-    if isinstance(handle, int):
-        return obj[handle]
+    # if it's a method
+    if isinstance(attr, (
+        BuiltinFunctionType, BuiltinMethodType,
+        FunctionType, MethodType,
+    )):
+        # call it and use return value
+        return attr()
 
-    # dict-like object
-    if hasattr(obj, 'keys'):
-        return obj[handle]
-
-    # object attribute or method
-    if hasattr(obj, handle):
-        attr = getattr(obj, handle)
-
-        # if it's a method, call it
-        if isinstance(attr, (
-            BuiltinFunctionType, BuiltinMethodType,
-            FunctionType, MethodType,
-        )):
-            # use return value
-            return attr()
-
-        # class attribute
-        return attr
-
-    # last chance
-    if hasattr(obj, '__getitem__'):
-        return obj[handle]
-
-    raise KeyError(handle)
+    # otherwise, it's a class attribute
+    return attr
--- tests/PluckPathTest.py
+++ tests/PluckPathTest.py
@@ -5,26 +5,118 @@ import sys
 import unittest
 
 sys.path = [ os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) ] + sys.path
-from pluckit.pluckit import pluckit
+from pluckit.pluckit import pluckit, path_regex
+
 
 
 class PluckPathTest(unittest.TestCase):
-    def test(self):
-        data = { 'a': [ { 'v': 1 }, 2 ] }
+    def test_path_regex(self):
+        def match_path(expected, path):
+            steps = []
+
+            while path:
+                match = path_regex.match(path)
+                self.assertTrue(match)
+
+                handle = match.group('handle') or match.group('subscript')
+                steps.append(handle)
+                path = path[len(match.group()):]
+            self.assertEqual(expected, steps)
+
+        match_path([ 'a' ], 'a')
+        match_path([ 'a' ], '[a]')
+        match_path([ '"a"' ], '["a"]')
+        match_path([ "'a'" ], "['a']")
+        match_path([ '0' ], '0')
+
+        match_path([ 'a', 'b' ], 'a.b')
+        match_path([ 'a', '0' ], 'a.0')
+
+        match_path([ '*' ], '*')
+        match_path([ '*' ], '[*]')
+        match_path([ 'a', '*' ], 'a.*')
+        match_path([ 'a', '*' ], 'a[*]')
+        match_path([ '*', 'a' ], '*.a')
+
+
+    def test_path_errors(self):
+        def bad_path(path):
+            self.assertFalse(path_regex.match(path))
+
+        bad_path('')
+
+        bad_path('.')
+        bad_path('[]')
+
+        bad_path('[')
+        bad_path(']')
+        bad_path('.[')
+        bad_path('.]')
+
+        bad_path('..')
+        bad_path('.[]')
+        bad_path('.[a]')
+        bad_path('.[0]')
+
+        with self.assertRaises(ValueError):
+            pluckit([ 0 ], '0.')
+
+        with self.assertRaises(ValueError):
+            pluckit([], '*.')
+
+        with self.assertRaises(ValueError):
+            pluckit([], '[')
+
+        with self.assertRaises(ValueError):
+            pluckit([], '*[')
+
+
+    def test_basic(self):
+        data = [ [ 1, 2 ], [ 3 ], 4 ]
+
+        self.assertEqual(1, pluckit(data, '0[0]'))
+        self.assertEqual(2, pluckit(data, '0[1]'))
+        self.assertEqual(3, pluckit(data, '1[0]'))
+
+        self.assertEqual(4, pluckit(data, '-1'))
+        self.assertEqual(2, pluckit(data, '0[-1]'))
+
+
+        data = { 'a': { 'b': 1 }, 'c': { 'd': 1 } }
+        self.assertEqual(1, pluckit(data, 'a["b"]'))
+        self.assertEqual(1, pluckit(data, "a['b']"))
+
+
+    def test_fns(self):
+        class Foo:
+            def foo(self):
+                return 'foo'
+
+        data = { 'a': { 'b': 1 }, 'c': { 'd': Foo() } }
 
         self.assertEqual(
-            1,
-            pluckit(data, 'a.0.v'),
+            [ 'a', 'c' ],
+            list(pluckit(data, '.keys'))
         )
 
         self.assertEqual(
-            1,
-            pluckit(data, 'a[0][v]'),
+            [ 'b' ],
+            list(pluckit(data, 'a.keys'))
         )
 
         self.assertEqual(
-            1,
-            pluckit(data, 'a[0]["v"]'),
+            [ 1 ],
+            list(pluckit(data, 'a.values'))
+        )
+
+        self.assertEqual(
+            'foo',
+            pluckit(data, 'c[d].foo')
+        )
+
+        self.assertEqual(
+            'Foo',
+            pluckit(data, 'c[d].__class__.__name__')
         )
 
 
@@ -34,15 +126,110 @@ class PluckPathTest(unittest.TestCase):
         with self.assertRaises(IndexError):
             pluckit(data, 'a[0]')
 
-        with self.assertRaises(IndexError):
+        with self.assertRaises(AttributeError):
             pluckit(data, 'a.0')
 
-        with self.assertRaises(TypeError):
+        with self.assertRaises(AttributeError):
             pluckit(data, 'a.z')
 
         with self.assertRaises(KeyError):
             pluckit(data, 'z')
 
+        with self.assertRaises(AttributeError):
+            pluckit(data, '.z')
+
+
+    def test_empty_list(self):
+        with self.assertRaises(IndexError):
+            pluckit([], '0')
+        with self.assertRaises(IndexError):
+            pluckit([], '-1')
+        with self.assertRaises(AttributeError):
+            pluckit([], '.0')
+
+
+    def test_empty_dict(self):
+        with self.assertRaises(KeyError):
+            pluckit({}, 'a')
+
+        with self.assertRaises(KeyError):
+            pluckit({}, '0')
+
+
+    def test_wildcard(self):
+        data = [ [ 1 ], [ 2 ] ]
+        self.assertEqual(data, pluckit(data, '*'))
+        self.assertEqual(data, pluckit(data, '.*'))
+
+        self.assertEqual(data, pluckit(data, '*.*'))
+        self.assertEqual(data, pluckit(data, '*[*]'))
+        self.assertEqual(data, pluckit(data, '.*.*'))
+
+        self.assertEqual([ 1, 2 ], pluckit(data, '*[0]'))
+
+
+        data = { 'a': { 'b': 1 } }
+        self.assertEqual([{ 'b': 1 }], pluckit(data, '*'))
+        self.assertEqual([{ 'b': 1 }], pluckit(data, '.*'))
+
+        self.assertEqual([ 1 ], pluckit(data, '*[b]'))
+        self.assertEqual([ 1 ], pluckit(data, 'a[*]'))
+
+        self.assertEqual([ [ 1 ] ], pluckit(data, '*[*]'))
+        self.assertEqual([ [ 1 ] ], pluckit(data, '*.*'))
+
+
+    def test_wildcard_advanced(self):
+        data = {
+            'a': { 'k': 1, 'v': [ 1, 2 ] },
+            'b': { 'k': 2, 'v': [ 3, 4 ] },
+        }
+
+        self.assertEqual(
+            [ 1, 2 ],
+            pluckit(data, '*[k]')
+        )
+
+        self.assertEqual(
+            [ [ 1, 2 ], [ 3, 4 ] ],
+            pluckit(data, '*[v]')
+        )
+
+        self.assertEqual(
+            [ [ 1, 2 ], [ 3, 4 ] ],
+            pluckit(data, '*[v]*')
+        )
+
+        self.assertEqual(
+            [ 1, 3 ],
+            pluckit(data, '*[v][0]')
+        )
+
+
+    def test_wildcard_empty(self):
+        self.assertEqual([], pluckit([], '*'))
+        self.assertEqual([], pluckit({}, '*'))
+
+        self.assertEqual([], pluckit([], '*.*'))
+        self.assertEqual([], pluckit({}, '*.*'))
+
+        self.assertEqual([], pluckit([], '*.a'))
+
+
+    def test_wildcard_errors(self):
+        data = { 'a': [], 'b': 2 }
+
+        with self.assertRaises(KeyError):
+            pluckit(data, '["*"]')
+
+        with self.assertRaises(TypeError):
+            pluckit(data, 'a["*"]')
+
+        with self.assertRaises(TypeError):
+            pluckit(data, 'b.*')
+
+        with self.assertRaises(ValueError):
+            pluckit(data, '*.')
 
 
 
